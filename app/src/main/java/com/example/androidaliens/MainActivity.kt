8023fb1bb2b3cd6f84e49f65ee82069046d22cf5
ibarbylev <ibarbylev@gmail.com>
2023-10-02 22:32:27 +0300

    add file android_alien.png

--- app/src/main/java/com/example/androidaliens/MainActivity.kt
+++ app/src/main/java/com/example/androidaliens/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.androidaliens
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidaliens.ui.theme.AndroidAliensTheme
 
@@ -31,10 +34,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+//        Text(
+//            text = "Hello $name!",
+//            modifier = modifier
+//        )
+        Image(
+            painter = painterResource(R.drawable.adnroid_alien),
+            contentDescription = null
+        )
+    }
 }
 
 @Preview(showBackground = true)
